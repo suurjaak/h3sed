67f8dd9bf0c67e7f83b93c02037fb0ea18af7751
Erki Suurjaak <erki@lap.ee>
2022-01-20 20:24:10 +0200

    v1.0 checkpoint.

--- src/h3sed/conf.py
+++ src/h3sed/conf.py
@@ -25,7 +25,7 @@ import sys
 """Program title, version number and version date."""
 Name = "h3sed"
 Title = "Heroes3 Savegame Editor"
-Version = "1.0.dev51"
+Version = "1.0"
 VersionDate = "19.01.2022"
 
 if getattr(sys, "frozen", False):
