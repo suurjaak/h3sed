df36a9ba26b12d4469a6cf000447ab1508bd8baf
Erki Suurjaak <erki@lap.ee>
2024-06-12 10:04:44 +0300

    Fix Orb of Tempestuous Fire being named Orb of Tempestous Fire.

--- src/h3sed/conf.py
+++ src/h3sed/conf.py
@@ -26,7 +26,7 @@ import sys
 """Program title, version number and version date."""
 Name = "h3sed"
 Title = "Heroes3 Savegame Editor"
-Version = "2.3.1.dev2"
+Version = "2.3.1.dev3"
 VersionDate = "12.06.2024"
 
 Frozen = getattr(sys, "frozen", False)
