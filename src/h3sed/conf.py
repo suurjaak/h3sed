952f8c2741aefaa4525033576ef3c4b30bb7c1d7
Erki Suurjaak <erki@lap.ee>
2026-04-03 21:24:07 +0300

    Fix error on adding skill when no skill selected.

--- src/h3sed/conf.py
+++ src/h3sed/conf.py
@@ -26,7 +26,7 @@ import sys
 """Program title, version number and version date."""
 Name = "h3sed"
 Title = "Heroes3 Savegame Editor"
-Version = "3.6.dev16"
+Version = "3.6.dev17"
 VersionDate = "03.04.2026"
 
 Frozen = getattr(sys, "frozen", False)
